46f5c4c9cd30368ceca2a362dd456356879e9bd2
Lapkalab <notlabua@gmail.com>
2022-11-16 18:40:48 +0200

    add addition

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,5 +5,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("My git app. Puper");
+
+        int a = 8;
+        int b = 2;
+        int c = add (a, b);
+        System.out.println(c);
     }
+    private static int add (int a, int b){
+    return  a+b;
+    }
+
 }
